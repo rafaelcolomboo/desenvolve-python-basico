d1ae44958f7d94d6410ad805fc97c718469fff90
rafaelcolomboo <rafael.colombo@pditabira.com>
2024-06-19 20:07:05 -0300

    Add files via upload

---
+++ modulo2/aula2_questao1.py
@@ -0,0 +1,7 @@
+x=5
+y=2
+z=x/y
+print(type(x))
+print(type(y))
+print(z)
+print(type(z))
---
+++ modulo2/aula2_questao4.py
@@ -0,0 +1,9 @@
+juros = 1.01
+saldo = 500.0
+
+saldo *= juros
+saldo *= juros
+saldo *= juros
+
+print("Após 3 meses meu novo saldo é")
+print(saldo)
