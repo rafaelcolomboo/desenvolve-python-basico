9283294247de141aa3968d156aa9058dc1de0827
rafaelcolomboo <rafael.colombo@pditabira.com>
2024-07-10 23:07:49 -0300

    Add files via upload

---
+++ modulo4/aula1_questão3.py
@@ -0,0 +1,14 @@
+n1= int(input("Digite um número:"))
+n2= int(input("Digite um número:"))
+n3= int(input("Digite um número:"))
+
+m= (n1+n2+n3)/3
+
+if m>=60:
+    print("Aprovado")
+elif m>=40:
+    print("Recuperação")
+else:
+    print("Reprovado")
+
+print("Fim")
