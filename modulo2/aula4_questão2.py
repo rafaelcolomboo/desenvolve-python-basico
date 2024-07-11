5ab204ef8e38c76f58aeaa7869c8af05d9c2a4e6
rafaelcolomboo <rafael.colombo@pditabira.com>
2024-07-10 23:03:51 -0300

    Add files via upload

---
+++ modulo2/aula4_questão2.py
@@ -0,0 +1,8 @@
+#Entradas de dados
+Temperatura_fahrenheit= F= int(input("A temperatura em fahreint é de:"))
+
+#O processo de conversão de F para C
+C=(F-32)*(5/9)
+
+#Impressão dos dados
+print(f"{F} graus Fahrenheit são {int(C)} graus Celsius")
