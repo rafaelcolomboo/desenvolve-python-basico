9483de24986e402a1cc4b4630966c3bd4dc4a9b9
rafaelcolomboo <rafael.colombo@pditabira.com>
2024-07-10 23:14:14 -0300

    Add files via upload

---
+++ modulo7/aula1_questao2.py
@@ -0,0 +1,5 @@
+primeiro_nome = input("Digite seu primeiro nome: ")
+sobrenome = input("Digite seu sobrenome: ")
+nome_completo = primeiro_nome + " " + sobrenome
+
+print(f"Bem-vinda, {nome_completo}!")
