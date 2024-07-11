ffa6851adbb95deefa01703e8a48a451fddc65ae
rafaelcolomboo <rafael.colombo@pditabira.com>
2024-07-10 23:06:01 -0300

    Add files via upload

---
+++ modulo3/aula4_questão2.py
@@ -0,0 +1,11 @@
+a= int(input("Avalie o filme de 1 a 5:"))
+if a == 5:
+    print("Excelente")
+if a == 4:
+    print("Muito Bom")
+if a == 3:
+    print("Bom")
+if a == 2:
+    print("Regular")
+if a == 1:
+    print("Ruim")         
